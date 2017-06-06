bb54e5046b32443d9d180165bf1537793b802eab
zhuxy <zhuxyo@163.com>
2017-06-06 22:58:40 +0800

    change

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -315,7 +315,7 @@ values."
    ;; `trailing' to delete only the whitespace at end of lines, `changed'to
    ;; delete only whitespace for changed lines or `nil' to disable cleanup.
    ;; (default nil)
-   dotspacemacs-whitespace-cleanup 'trailing
+   dotspacemacs-whitespace-cleanup nil
    ))
 
 (defun dotspacemacs/user-init ()
