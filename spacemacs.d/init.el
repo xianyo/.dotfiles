82b957a7ba76591a173807e839f1e6d871c53089
zhuxy <zhuxy@paxsz.com>
2017-11-09 11:00:53 +0800

    add pyim dict

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -699,6 +699,21 @@ you should place your code here."
     (setq default-input-method "pyim")
     (setq pyim-default-scheme 'quanpin)
 
+    ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+    ;; 我自己使用的中英文动态切换规则是：
+    ;; 1. 光标只有在注释里面时，才可以输入中文。
+    ;; 2. 光标前是汉字字符时，才能输入中文。
+    ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+    ;; (setq-default pyim-english-input-switch-functions
+    ;;               '(pyim-probe-dynamic-english
+    ;;                 pyim-probe-isearch-mode
+    ;;                 pyim-probe-program-mode
+    ;;                 pyim-probe-org-structure-template))
+
+    ;; (setq-default pyim-punctuation-half-width-functions
+    ;;               '(pyim-probe-punctuation-line-beginning
+    ;;                 pyim-probe-punctuation-after-punctuation))
+
     ;; 开启拼音搜索功能
     ;; (setq pyim-isearch-mode t)
 
@@ -708,9 +723,14 @@ you should place your code here."
     ;; 选词框显示5个候选词
     (setq pyim-page-length 5)
 
+    (setq pyim-dicts
+          '((:name "bigdict" :file "~/.dotfiles/spacemacs.d/pyim/pyim-bigdict.pyim")))
+
     ;; 让 Emacs 启动时自动加载 pyim 词库
     (add-hook 'emacs-startup-hook
               #'(lambda () (pyim-restart-1 t)))
+
+    ;; (global-set-key (kbd "M-i") 'pyim-convert-code-at-point)
 
 )
 
